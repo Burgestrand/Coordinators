5d8796492d01660d94e5abf2051428aafb2a7925
Kim Burge Strand <kim@burgestrand.se>
2017-04-13 18:39:36 +0200

    Add auth to AppCoordinator.

--- Coordinators.playground/Sources/AppCoordinator.swift
+++ Coordinators.playground/Sources/AppCoordinator.swift
@@ -3,27 +3,53 @@ import UIKit
 public class AppCoordinator: Coordinator {
     enum Flow {
         case tutorial(TutorialCoordinator)
-        //        case auth(AuthCoordinator)
+        case auth(AuthCoordinator)
         //        case main(MainCoordinator)
+
+        func start() {
+            switch self {
+            case .tutorial(let coordinator):
+                coordinator.start()
+            case .auth(let coordinator):
+                coordinator.start()
+            }
+        }
     }
 
-    let window: UIWindow
+    private let window: UIWindow
+    var current: Flow! {
+        didSet {
+            debugPrint(current)
+            current.start()
+        }
+    }
 
     required public init(_ window: UIWindow, completion: @escaping (Void) -> ()) {
         self.window = window
     }
 
+    // App state
+    var firstLaunch = true
     var userName: String?
-    var current: Flow!
 
     public func start() {
-        if let userName = userName {
-            fatalError("NO")
-        } else {
-            let tutorial = TutorialCoordinator(window) { _ in }
-            tutorial.start()
-        }
-
+        current = nextFlow()
         window.makeKeyAndVisible()
+    }
+
+    private func nextFlow() -> Flow {
+        if firstLaunch {
+            firstLaunch = false
+            let coordinator = TutorialCoordinator(window) { [unowned self] tutorialCompleted in
+                debugPrint("Tutorial completed", tutorialCompleted)
+                self.current = self.nextFlow()
+            }
+            return .tutorial(coordinator)
+        } else {
+            let coordinator = AuthCoordinator(window) { [unowned self] userName in
+                debugPrint("User signed in", userName)
+            }
+            return .auth(coordinator)
+        }
     }
 }
