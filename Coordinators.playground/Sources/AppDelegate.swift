0511fbd0cf008ead6dadfc74d442ead43c2ff049
Kim Burge Strand <kim@burgestrand.se>
2017-04-14 09:43:50 +0200

    Having a window frame causes changes of rootVC to go black screen.

--- Coordinators.playground/Sources/AppDelegate.swift
+++ Coordinators.playground/Sources/AppDelegate.swift
@@ -7,7 +7,7 @@ public class AppDelegate {
     }
 
     public func applicationDidFinishLaunching() -> UIWindow {
-        let window = UIWindow(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+        let window = UIWindow()
         coordinator = AppCoordinator(window)
         coordinator.start()
         return window
